d36adb65c4aabcd9cbcc6abef51d55918f1a2d92
Vrushank Patel <vrushankpatel5@gmail.com>
2021-02-08 19:39:12 +0530

    modified web config

--- src/main/java/com/quortexservice/quortex/config/WebConfig.java
+++ src/main/java/com/quortexservice/quortex/config/WebConfig.java
@@ -9,11 +9,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
  
-    @Override
+    /* @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         	.allowedOrigins("*")
         	.allowedMethods("*")
         	.allowedHeaders("*");
+    } */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> {
+            CorsConfiguration cors = new CorsConfiguration();
+            cors.setAllowedOrigins(
+                    Lists.newArrayList("*"));
+            cors.setAllowedMethods(Lists.newArrayList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(Lists.newArrayList("*"));
+            return cors;
+        }).and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("")
+                .permitAll().and()
+                .addFilterBefore(setLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
